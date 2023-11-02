5072252c3f8063ad72dbc3da42b4f6fc627c72c2
Yatichapat Dechaeamsakul <yatichapat.d@ku.th>
2023-11-02 10:53:38 +0700

    Add delete method and edit deposit function

--- bank_account.py
+++ bank_account.py
@@ -9,8 +9,16 @@ class AccountDB:
         else:
             print(account, "Duplicated account; nothing to be insert")
 
+    def delete_account(self, num):
+        index = self.__search_private(num)
+        if index != -1:
+            delete_acc = self.account_database.pop(index)
+            print("Deleting account:", delete_acc.account_number)
+        else:
+            print(num, "invalid account number; nothing to be deleted.")
+
     def __search_private(self, account_num):  # search each account in private attribute
-        for i in range(len(self.account_database)):
+        for i, account in enumerate(self.account_database):
             if self.account_database[i].account_number == account_num:
                 return i
         return -1
@@ -26,6 +34,14 @@ class AccountDB:
         for account in self.account_database:
             s += str(account) + ", "
         return s
+
+    def deposit_to_account(self, account_num, amount):
+        account = self.search_public(account_num)
+        if account:
+            account.deposit(amount)
+            print("Deposited", amount, "to account:", account_num)
+        else:
+            print(account_num, "invalid account number; nothing to be deposited.")
 
 
 class Account:
@@ -47,21 +63,27 @@ class Account:
             self.balance) + '}'
 
 
+# Create accounts and account DB
 account1 = Account("0000", "saving", "David Patterson", 1000)
 account2 = Account("0001", "checking", "John Hennessy", 2000)
 account3 = Account("0003", "saving", "Mark Hill", 3000)
 account4 = Account("0004", "saving", "David Wood", 4000)
-account5 = Account("0004", "saving", "David Wood", 4000)
+account5 = Account("0005", "saving", "Alice Johnson", 5000)
+
 my_account_DB = AccountDB()
+
+# Insert accounts
 my_account_DB.insert(account1)
 my_account_DB.insert(account2)
 my_account_DB.insert(account3)
 my_account_DB.insert(account4)
 my_account_DB.insert(account5)
 print(my_account_DB)
-my_account_DB.search_public("0003").deposit(50)
+
+# Deposit to account
+my_account_DB.deposit_to_account("0010", 50)
 print(my_account_DB)
-my_account_DB.search_public("0003").withdraw(100)
-print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
+
+# Delete an account
+my_account_DB.delete_account("0003")
 print(my_account_DB)
